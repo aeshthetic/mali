806f5750631fa72b57c3fe68ec8d08b8714c98e9
aeshthetic <eabhijay@protonmail.com>
2018-02-27 15:51:00 -0500

    Fixed order of comments in Generic and made function descriptions XML Docs

--- Dixie/Util/Generic.fs
+++ Dixie/Util/Generic.fs
@@ -1,12 +1,11 @@
 ﻿module Dixie.Util.Generic
-open System
 open System.Reflection
 open Dixie.Util.Attributes
 open Dixie.Util.Types
 
 // val typeName: Type -> string
-// Generates a human-readable name of a Type object
 // t: the type to generate a name for
+/// Generates a human-readable name of a Type object
 let rec typeName (t: System.Type) =
     let name = (t.Name.Split '`').[0]
 
@@ -26,17 +25,17 @@ let rec typeName (t: System.Type) =
     | _ -> sprintf "%s %s" parameterString name
 
 // val isList<'Record>: string -> bool
-// Checks whether a particular member of a record type is a list
 // 'Record: the record type whose member is being checked
 // memberName: the name of the member being checked
+/// Checks whether a particular member of a record type is a list
 let isList<'Record> (memberName: string) =
     let typeOfMember = typeof<'Record>.GetProperty(memberName).PropertyType in
     let typeInfo = typeOfMember.GetGenericTypeDefinition() in
     typeInfo = typedefof<list<_>>
 
 // val listPropType: PropertyInfo -> Type
-// Returns the type of a list property
 // prop: the list property (e.g. 'T list) whose type is being returned
+/// Returns the type of a list property
 let listPropType (prop: PropertyInfo) =
     prop
         .PropertyType
@@ -44,11 +43,11 @@ let listPropType (prop: PropertyInfo) =
         |> Array.head
 
 // val mapType: Type -> Table list
-// Generates a list of Tables from a record type
-// Note: currently dysfunctional. This should essentially "map" a record type to
-// a relational table schema that can be used to generate real RDB tables. This is
-// currently a goal that has yet to be reached.
 // t: the record type to map
+/// Generates a list of Tables from a record type
+/// Note: currently dysfunctional. This should essentially "map" a record type to
+/// a relational table schema that can be used to generate real RDB tables. This is
+/// currently a goal that has yet to be reached.
 let rec mapType (t: System.Type) =
     let props = t.GetProperties()
 
@@ -69,10 +68,10 @@ let rec mapType (t: System.Type) =
 
     let tables = [{name = t.Name; schema = Map.ofArray <| Array.zip names types}]
 
-    // This is a result of the nature of One to Many relationships
-    // in that they require a table containing the "many" objects
-    // to have a column referencing the "one" object. Therefore
-    // a table must be created for each type of OTM list
+    (* This is a result of the nature of One to Many relationships
+     in that they require a table containing the "many" objects
+     to have a column referencing the "one" object. Therefore
+     a table must be created for each type of OTM list *)
     let mapOneToMany = listPropType >> mapType
 
     match oneToManys with
