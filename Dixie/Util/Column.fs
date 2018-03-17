d91504c1f639d6fd277d1394accce8d91c1e3abc
Abhijay Eshwar <eabhijay@gmail.com>
2018-03-17 15:44:01 -0400

    Added column utilities

---
+++ Dixie/Util/Column.fs
@@ -0,0 +1,18 @@
+module Dixie.Util.Column
+open Dixie.Util.Types
+
+// val stringOf: DBMS -> Column -> string
+// dbms: the DBMS of the connection for which the string representation should be formatted
+// col: the column being represented by the output string
+/// Returns a string representing a column in the way it would be in a CREATE statement
+let stringOf dbms col =
+    let stringFunction =
+        match dbms with
+        | PostgreSQL -> DbType.postgres
+    let typeString = stringFunction col.dataType
+    let constraints =
+        col.constraints
+        |> Array.map (fun it -> it.ToString())
+        |> String.concat " "
+
+    sprintf "%s %s %s" (col.name) (typeString) (constraints)
